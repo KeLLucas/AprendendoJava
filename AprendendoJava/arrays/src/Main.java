a28f317ccbcdf6c3113d67b8cc871da3ab276524
KeLLucas <Kelvimlucas.dev@proton.me>
2025-05-29 10:46:54 -0300

    teste

--- AprendendoJava/arrays/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    static public void main(String[] args) {
-
-        //array = a collection of values of the same data type
-        //          * think of it as a variable that can store more than 1 value *
-
-
-    }
-}
