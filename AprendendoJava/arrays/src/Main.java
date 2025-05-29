445b6b740208d7e6eaa349e9c1d8506a4889cbbd
KeLLucas <Kelvimlucas.dev@proton.me>
2025-05-29 10:36:18 -0300

    criando repositorio java

---
+++ AprendendoJava/arrays/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static public void main(String[] args) {
+
+        //array = a collection of values of the same data type
+        //          * think of it as a variable that can store more than 1 value *
+
+
+    }
+}
